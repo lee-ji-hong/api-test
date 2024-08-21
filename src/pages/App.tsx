8ebaa309859c86535ca5babf6eac0ed6449eabc3
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-22 00:48:46 +0900

    Chore: "react-query 설치 밎 세팅"

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
 import HomePage from "./HomePage";
 import ScssExample from "./scss-example";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="scss-example" element={<ScssExample />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="scss-example" element={<ScssExample />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
