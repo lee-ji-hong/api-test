d40cae4b10fc0871e1327086dbca4a719686d16b
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-27 23:17:15 +0900

    Feat: section, spacing 컴포넌트 생성"

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Spacing from "@/components/shared/Spacing";
+import Section01 from "@/components/shared/Section01";
 
 const HomePage = () => {
-  return <div>홈페이지</div>;
+  return (
+    <div>
+      <Spacing size={32} />
+      <Section01 title="테스트" />
+    </div>
+  );
 };
 
 export default HomePage;
