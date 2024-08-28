c6a1f2c8920e642c765bcd87fae3e101321ceb11
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-29 02:14:43 +0900

    Feat: "input창 입력 값 기능"
    - money array를 ts로 분리
    - money key-value 형식으로 관리
    3. handleChangeValue, onChange이벤트발생 시 입력 값 누적
    4. input값에는 숫자만 입력되도록 제한
    5. input 숫자 단위 추가
    6. 최소 100만원 에서 최대 20억까지 입력 가능
    7. input창에 만원 문자 추가

--- src/pages/DepositEntryPage/index.tsx
+++ src/pages/DepositEntryPage/index.tsx
@@ -1,28 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import Text from "@/components/shared/Text";
-import DepositInput from "@/components/shared/DepositInput";
-import Spacing from "@/components/shared/Spacing";
-import Button from "@/components/shared/Button";
-import BadgeList from "@/components/shared/BadgeList";
-
+import { MONEY } from "@/constants/money";
 import classNames from "classnames/bind";
 import styles from "./DepositEntryPage.module.scss";
 const cx = classNames.bind(styles);
 
+import DepositInput from "@/components/shared/DepositInput";
+import BadgeList from "@/components/shared/BadgeList";
+import Spacing from "@/components/shared/Spacing";
+import Button from "@/components/shared/Button";
+import Text from "@/components/shared/Text";
+
 const DepositEntryPage = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState<number>("");
+  const moneyKeys = Object.keys(MONEY);
   const navigate = useNavigate();
-  const money = ["+10만", "+100만", "+1000만", "+1억"];
 
-  // Input 포커스 핸들러
   const handleFocus = () => setIsInputFocused(true);
   const handleBlur = () => setIsInputFocused(false);
 
   const handleChangeValue = (value: string) => {
-    console.log(value);
-    setInputValue(value);
+    const amount = MONEY[value];
+    setInputValue((prevValue) => Math.min(prevValue + amount, 200000));
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value.replace(/,/g, ""), 10);
+    if (!isNaN(newValue)) {
+      setInputValue(Math.min(newValue, 200000));
+    }
+  };
+
+  const formatNumber = (number: number) => {
+    return `${new Intl.NumberFormat().format(number)}만원`;
   };
 
   return (
@@ -35,11 +46,12 @@ const DepositEntryPage = () => {
         placeholder="0만원"
         onFocus={handleFocus}
         onBlur={handleBlur}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        value={formatNumber(inputValue)}
+        onChange={handleInputChange}
+        inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
       />
       <Spacing size={58} />
-      <BadgeList list={money} onClick={handleChangeValue} />
+      <BadgeList list={moneyKeys} onClick={handleChangeValue} />
       <Button
         onClick={() => navigate("/deposit-entry")}
         title="전월세 대출 상품 확인하기"
