6e93824aeaf70495a0d33c0ec9880e132b881721
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-28 15:43:20 +0900

    Feat: "온보딩페이지에 들어갈 이미지와 나머지 박스 정보 추가"

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,7 @@
 import Onboarding_1 from "@/assets/images/onboarding_1.png";
 import Onboarding_2 from "@/assets/images/onboarding_2.png";
+import Onboarding_3 from "@/assets/images/onboarding_3.png";
+import Onboarding_4 from "@/assets/images/onboarding_4.png";
 
 export const IMAGES = {
   Onboarding_1: {
@@ -9,5 +11,13 @@ export const IMAGES = {
   Onboarding_2: {
     src: Onboarding_2,
     alt: "전구",
+  },
+  Onboarding_3: {
+    src: Onboarding_3,
+    alt: "문서",
+  },
+  Onboarding_4: {
+    src: Onboarding_4,
+    alt: "말풍선",
   },
 };
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Spacing from "@/components/shared/Spacing";
 import Section01 from "@/components/shared/Section01";
 import Image from "@/components/shared/Image";
+import Text from "@/components/shared/Text";
+import Box from "@/components/shared/Box";
 
 import { IMAGES } from "@/constants/images";
 import classNames from "classnames/bind";
@@ -11,15 +13,39 @@ const cx = classNames.bind(styles);
 const HomePage = () => {
   console.log(IMAGES.Onboarding_1);
   return (
-    <div className={cx("txt-box")}>
+    <div>
       <Spacing size={82} />
       <Section01 title="당신을 위한/n최적의 전세 대출을/n찾아드릴게요">
         <Spacing size={18} />
-        <span>최신 정보를 바탕으로</span>
-        <span>맞춤형 대출 솔루션을 제공받으세요</span>
+        <Text className={cx("txt-box")} text="최신 정보를 바탕으로/n맞춤형 대출 솔루션을 제공받으세요" />
         <Spacing size={52} />
         <Image imageInfo={IMAGES?.Onboarding_1} />
       </Section01>
+      <Box>
+        <Text
+          className={cx("txt-title")}
+          text="간단한 정보 입력으로/n맞춤형 전세 대출을 추천드려요"
+          highlight="맞춤형 전세 대출"
+        />
+        <Text className={cx("txt-sub")} text="설정한 전세 대출 금액에 따라/n최적의 조건을 추천드릴 수 있어요" />
+        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_2} />
+      </Box>
+      <Spacing size={34} />
+      <Box>
+        <Text className={cx("txt-title")} text="부스비용 및 기회비용까지/n리포트로 제공드려요" highlight="리포트" />
+        <Text className={cx("txt-sub")} text="숨겨진 비용까지 철저히 분석하여/n현명한 결정을 할 수 있어요" />
+        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_3} />
+      </Box>
+      <Spacing size={34} />
+      <Box>
+        <Text
+          className={cx("txt-title")}
+          text="선택한 대출 상품에 대해/n무료 상담까지 가능해요"
+          highlight="무료 상담"
+        />
+        <Text className={cx("txt-sub")} text="궁금한 점,/n다 물어보고 대출을 선택하세요" />
+        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_4} />
+      </Box>
     </div>
   );
 };
