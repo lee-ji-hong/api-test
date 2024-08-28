09bdba710c061fe5d73eeb5bdc1cbfbde107d2ec
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-28 21:49:30 +0900

    Feat: "보증금 입력 창 UI 생성"

---
+++ src/components/shared/DepositInput.tsx
@@ -0,0 +1,26 @@
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/system";
+
+const DepositInput = styled(TextField)({
+  width: "311px",
+  marginBottom: "16px",
+  "& .MuiInput-underline:before": {
+    border: "none",
+  },
+  "& .MuiInput-underline:hover:before": {
+    border: "none",
+  },
+  "& .MuiInput-input": {
+    fontFamily: "Pretendard",
+    height: "46px",
+    fontSize: "38px",
+    fontWeight: 700,
+    lineHeight: "45.35px",
+    textAlign: "center",
+    "&::placeholder": {
+      color: "#DADAE1",
+    },
+  },
+});
+
+export default DepositInput;
--- src/pages/DepositEntryPage/index.tsx
+++ src/pages/DepositEntryPage/index.tsx
@@ -1,9 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import Text from "@/components/shared/Text";
+import DepositInput from "@/components/shared/DepositInput";
+import Button from "@/components/shared/Button";
+import Spacing from "@/components/shared/Spacing";
+
 import classNames from "classnames/bind";
 import styles from "./DepositEntryPage.module.scss";
 const cx = classNames.bind(styles);
 
 const DepositEntryPage = () => {
-  return <div className={cx("container")}>DepositEntryPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={cx("container")}>
+      <Spacing size={179} />
+      <Text className={cx("txt-title")} text="전월세보증금은?" />
+      <DepositInput id="standard-basic" variant="standard" placeholder="0만원" />
+      <Button onClick={() => navigate("/deposit-entry")} title="전월세 대출 상품 확인하기" />
+      <Spacing size={14} />
+    </div>
+  );
 };
 
 export default DepositEntryPage;
