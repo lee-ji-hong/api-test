7a7f85fe535ab0ce7227fdf0491c345435e0c9e7
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-29 00:44:05 +0900

    Feat: "input버튼 입력하면 버튼 위치 올라감"

--- src/pages/DepositEntryPage/index.tsx
+++ src/pages/DepositEntryPage/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Text from "@/components/shared/Text";
 import DepositInput from "@/components/shared/DepositInput";
 import Spacing from "@/components/shared/Spacing";
@@ -10,16 +11,32 @@ import styles from "./DepositEntryPage.module.scss";
 const cx = classNames.bind(styles);
 
 const DepositEntryPage = () => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
   const navigate = useNavigate();
   const money = ["+10만", "+100만", "+1000만", "+1억"];
+
+  // Input 포커스 핸들러
+  const handleFocus = () => setIsInputFocused(true);
+  const handleBlur = () => setIsInputFocused(false);
+
   return (
     <div className={cx("container")}>
       <Spacing size={179} />
       <Text className={cx("txt-title")} text="전월세보증금은?" />
-      <DepositInput id="standard-basic" variant="standard" placeholder="0만원" />
+      <DepositInput
+        id="standard-basic"
+        variant="standard"
+        placeholder="0만원"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
       <Spacing size={58} />
       <BadgeList list={money} />
-      <Button onClick={() => navigate("/deposit-entry")} title="전월세 대출 상품 확인하기" />
+      <Button
+        onClick={() => navigate("/deposit-entry")}
+        title="전월세 대출 상품 확인하기"
+        className={cx("fixed-button", { "with-input-focus": isInputFocused })}
+      />
     </div>
   );
 };
