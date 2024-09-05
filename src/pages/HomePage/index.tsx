8f93642514ec7a18718d43158aece36be2f3f96a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-05 16:28:43 +0900

    Feat: "화면 전환 애니메이션 추가"

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,6 @@
+import { CSSTransition } from "react-transition-group";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Spacing from "@/components/shared/Spacing";
 import Section01 from "@/components/shared/Section01";
 import Image from "@/components/shared/Image";
@@ -12,48 +14,67 @@ import styles from "./HomePage.module.scss";
 const cx = classNames.bind(styles);
 
 const HomePage = () => {
+  const [showPage, setShowPage] = useState(true);
   const navigate = useNavigate();
 
+  const handleCancelClick = () => {
+    setShowPage(false);
+    setTimeout(() => {
+      navigate("/deposit-entry");
+    }, 200);
+  };
+
   return (
-    <div data-testid="home-page">
-      <Section01 className={cx("section")} title="당신을 위한/n최적의 전세 대출을/n찾아드릴게요">
-        <div onClick={() => navigate("/deposit-entry")}>
-          <Image className={cx("cancel")} imageInfo={IMAGES?.Cancel_btn} />
-        </div>
-        <Spacing size={18} />
-        <Text className={cx("txt-box")} text="최신 정보를 바탕으로/n맞춤형 대출 솔루션을 제공받으세요" />
-        <Spacing size={52} />
-        <Image className={cx("top-img")} imageInfo={IMAGES?.Onboarding_1} />
-      </Section01>
-      <Box>
-        <Text
-          className={cx("txt-title")}
-          text="간단한 정보 입력으로/n맞춤형 전세 대출을 추천드려요"
-          highlight="맞춤형 전세 대출"
-        />
-        <Text className={cx("txt-sub")} text="설정한 전세 대출 금액에 따라/n최적의 조건을 추천드릴 수 있어요" />
-        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_2} />
-      </Box>
-      <Spacing size={34} />
-      <Box>
-        <Text className={cx("txt-title")} text="부스비용 및 기회비용까지/n리포트로 제공드려요" highlight="리포트" />
-        <Text className={cx("txt-sub")} text="숨겨진 비용까지 철저히 분석하여/n현명한 결정을 할 수 있어요" />
-        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_3} />
-      </Box>
-      <Spacing size={34} />
-      <Box>
-        <Text
-          className={cx("txt-title")}
-          text="선택한 대출 상품에 대해/n무료 상담까지 가능해요"
-          highlight="무료 상담"
-        />
-        <Text className={cx("txt-sub")} text="궁금한 점,/n다 물어보고 대출을 선택하세요" />
-        <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_4} />
-      </Box>
-      <Spacing size={60} />
-      <Button onClick={() => navigate("/deposit-entry")} title="전월세 대출 상품 확인하러 가기" />
-      <Spacing size={14} />
-    </div>
+    <CSSTransition
+      in={showPage}
+      timeout={500}
+      classNames={{
+        enter: cx("page-enter"),
+        enterActive: cx("page-enter-active"),
+        exit: cx("page-exit"),
+        exitActive: cx("page-exit-active"),
+      }}
+      unmountOnExit>
+      <div data-testid="home-page">
+        <Section01 className={cx("section")} title="당신을 위한/n최적의 전세 대출을/n찾아드릴게요">
+          <div onClick={handleCancelClick}>
+            <Image className={cx("cancel")} imageInfo={IMAGES?.Cancel_btn} />
+          </div>
+          <Spacing size={18} />
+          <Text className={cx("txt-box")} text="최신 정보를 바탕으로/n맞춤형 대출 솔루션을 제공받으세요" />
+          <Spacing size={52} />
+          <Image className={cx("top-img")} imageInfo={IMAGES?.Onboarding_1} />
+        </Section01>
+        <Box>
+          <Text
+            className={cx("txt-title")}
+            text="간단한 정보 입력으로/n맞춤형 전세 대출을 추천드려요"
+            highlight="맞춤형 전세 대출"
+          />
+          <Text className={cx("txt-sub")} text="설정한 전세 대출 금액에 따라/n최적의 조건을 추천드릴 수 있어요" />
+          <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_2} />
+        </Box>
+        <Spacing size={34} />
+        <Box>
+          <Text className={cx("txt-title")} text="부스비용 및 기회비용까지/n리포트로 제공드려요" highlight="리포트" />
+          <Text className={cx("txt-sub")} text="숨겨진 비용까지 철저히 분석하여/n현명한 결정을 할 수 있어요" />
+          <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_3} />
+        </Box>
+        <Spacing size={34} />
+        <Box>
+          <Text
+            className={cx("txt-title")}
+            text="선택한 대출 상품에 대해/n무료 상담까지 가능해요"
+            highlight="무료 상담"
+          />
+          <Text className={cx("txt-sub")} text="궁금한 점,/n다 물어보고 대출을 선택하세요" />
+          <Image className={cx("img")} imageInfo={IMAGES?.Onboarding_4} />
+        </Box>
+        <Spacing size={60} />
+        <Button onClick={() => navigate("/deposit-entry")} title="전월세 대출 상품 확인하러 가기" />
+        <Spacing size={14} />
+      </div>
+    </CSSTransition>
   );
 };
 
