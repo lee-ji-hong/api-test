b1d0568229f3a39146349c49fadcbd3fe9fe38bb
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-06 13:55:02 +0900

    Feat: "보증금 결과 리스트 컴포넌트 생성"

---
+++ src/components/shared/DepositList.tsx
@@ -0,0 +1,38 @@
+import classNames from "classnames/bind";
+import styles from "./DepositList.module.scss";
+const cx = classNames.bind(styles);
+
+interface Item {
+  name: string;
+  rate: number;
+  money: string;
+}
+
+interface ListProps {
+  className?: string;
+  list?: Item[];
+}
+
+export const DepositList = ({ list, className }: ListProps) => {
+  return (
+    <div className={cx(["container", className])}>
+      {list.map((item, index) => (
+        <div key={index} className={cx("item")}>
+          <div className={cx("info")}>
+            <div className={cx("logo")}>
+              <img src="/path/to/logo.png" alt="logo" className={cx("logo-img")} /> {/* 로고 이미지 */}
+            </div>
+            <div>
+              <span className={cx("name")}>{item.name}</span>
+            </div>
+          </div>
+          <div className={cx("details")}>
+            <span className={cx("rate")}>{item.rate}%</span>
+            <span className={cx("money")}>{item.money}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default DepositList;
