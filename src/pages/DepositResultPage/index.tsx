0aaea6f43159093ab2c4efa33824475642963079
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-09 20:00:43 +0900

    Feat: "back 버튼 추가, 보증금 금액 입력 값 수정"

--- src/pages/DepositResultPage/index.tsx
+++ src/pages/DepositResultPage/index.tsx
@@ -1,18 +1,22 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { MOCK } from "./mock";
+
+import DepositList from "@/components/shared/DepositList";
+import Spacing from "@/components/shared/Spacing";
+import Header from "@/components/sections/Header";
+import Button from "@/components/shared/Button";
+import Text from "@/components/shared/Text";
+
+import { useInternalRouter } from "@/hooks/useInternalRouter";
 import classNames from "classnames/bind";
 import styles from "./DepositResultPage.module.scss";
 const cx = classNames.bind(styles);
 
-import DepositList from "@/components/shared/DepositList";
-import Spacing from "@/components/shared/Spacing";
-import Button from "@/components/shared/Button";
-import Text from "@/components/shared/Text";
-
 export const DepositResultPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const router = useInternalRouter();
   const { inputValue } = location.state || { inputValue: 0 };
 
   useEffect(() => {
@@ -21,23 +25,43 @@ export const DepositResultPage = () => {
     }
   }, [inputValue, navigate]);
 
+  const formatNumber = (number: number) => {
+    return `${new Intl.NumberFormat().format(number)}만원`;
+  };
+
+  const formatNumberWithUnits = (number: number): string => {
+    if (number >= 10000) {
+      const billion = Math.floor(number / 10000); // 억 단위 계산
+      const million = number % 10000; // 만 단위 계산
+      if (million > 0) {
+        return `${billion}억 ${new Intl.NumberFormat().format(million)}만원`;
+      }
+      return `${billion}억`;
+    }
+    return formatNumber(number); // 억 단위 미만
+  };
+
   return (
-    <div className={cx("container")}>
-      <Spacing size={26} />
-      <Text
-        className={cx("txt-title")}
-        text={`보증금 ${inputValue}만원을/n마련할 수 있는 상품이에요`}
-        highlight={`${inputValue}만원`}
-      />
-      <Spacing size={10} />
-      <Text className={cx("txt-sub")} text="추가 정보를 입력하고/n맞춤형 전월세대출을 알아보세요" />
-      <DepositList list={MOCK} color="white" />
-      <Button
-        className={cx("fixed-button")}
-        onClick={() => navigate("/deposit-entry")}
-        title="맞춤형 전월세대출 더 알아보기"
-      />
-    </div>
+    <>
+      <Header className={cx("cancel")} onRightClick={() => router.goBack()} right="Back_btn" />
+      <div className={cx("container")}>
+        <Spacing size={26} />
+        <Text
+          className={cx("txt-title")}
+          text={`보증금 ${formatNumberWithUnits(inputValue)}을/n마련할 수 있는 상품이에요`}
+          highlight={`${formatNumberWithUnits(inputValue)}`}
+        />
+        <Spacing size={10} />
+        <Text className={cx("txt-sub")} text="추가 정보를 입력하고/n맞춤형 전월세대출을 알아보세요" />
+        <Spacing size={35} />
+        <DepositList list={MOCK} color="white" />
+        <Button
+          className={cx("fixed-button")}
+          onClick={() => navigate("/deposit-entry")}
+          title="맞춤형 전월세대출 더 알아보기"
+        />
+      </div>
+    </>
   );
 };
 
