1e7a2cfc759d9a8236f7a3677d730c2812b891bf
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-10 14:50:28 +0900

    Feat: "글로벌 포탈 설정"

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import AppLayout from "../layout/AppLayout";
 import LoginPage from "./LoginPage";
+import { GlobalPortal } from "@/components/shared/GlobalPortal";
 import FullScreenMessage from "@/components/sections/FullScreenMessage";
 import CommunityWirtePage from "./CommunityWirtePage";
 import CommunityDetail from "./CommunityDetail";
-
 const HomePage = lazy(() => import("./HomePage"));
 const DepositEntryPage = lazy(() => import("./DepositEntryPage"));
 const DepositResultPage = lazy(() => import("./DepositResultPage"));
@@ -27,23 +27,23 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<FullScreenMessage type="loading" />}>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="deposit-entry" element={<DepositEntryPage />} />
-            <Route path="deposit-result" element={<DepositResultPage />} />
-            <Route path="test" element={<ApiPage />} />
-            <Route path="scss-example" element={<ScssExample />} />
-            <Route path="login-result" element={<LoginSuccessPage />} />
-            <Route path="login-page" element={<LoginPage />} />
-            <Route path="community" element={<CommunityPage />} />
-            <Route path="community/write" element={<CommunityWirtePage />} />
-            <Route path="community/detail" element={<CommunityDetail />} />
-            <Route path="loan-info-entry" element={<LoanInfoEntryPage />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <GlobalPortal.Provider>
+        <Suspense fallback={<FullScreenMessage type="loading" />}>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="deposit-entry" element={<DepositEntryPage />} />
+              <Route path="deposit-result" element={<DepositResultPage />} />
+              <Route path="test" element={<ApiPage />} />
+              <Route path="scss-example" element={<ScssExample />} />
+              <Route path="community" element={<CommunityPage />} />
+              <Route path="community/write" element={<CommunityWirtePage />} />
+              <Route path="community/detail" element={<CommunityDetail />} />
+              <Route path="loan-info-entry" element={<LoanInfoEntryPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </GlobalPortal.Provider>
     </QueryClientProvider>
   );
 }
