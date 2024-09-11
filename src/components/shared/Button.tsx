3cf1a0ee74d300e2ece15356c8a45d3c80de4a9a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-11 23:14:59 +0900

    Feat: "버튼 타입 추가"

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -5,20 +5,21 @@ import classNames from "classnames/bind";
 import styles from "./Button.module.scss";
 
 const cx = classNames.bind(styles);
-
+export type ButtonType = "button" | "submit" | "reset";
 interface ButtonProps {
   className?: string;
   title: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   bottom?: number;
+  type?: ButtonType;
 }
 
-const Button = ({ className, title, onClick, disabled, bottom = 0 }: ButtonProps) => {
+const Button = ({ className, title, onClick, disabled, bottom = 0, type = "button" }: ButtonProps) => {
   return (
     <GlobalPortal.Consumer>
       <div className={cx(["button-wrap", className])} style={{ bottom: `${bottom}px` }}>
-        <button className={cx("container")} onClick={onClick} disabled={disabled}>
+        <button className={cx("container")} onClick={onClick} disabled={disabled} type={type}>
           {title}
         </button>
       </div>
