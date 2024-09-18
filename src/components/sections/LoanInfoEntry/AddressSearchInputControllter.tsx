92088381c83b83b40ea336b13f476dcef01df5c1
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-18 13:26:27 +0900

    Feat: "주택정보 input"

--- src/components/sections/LoanInfoEntry/AddressSearchInputControllter.tsx
+++ src/components/sections/LoanInfoEntry/AddressSearchInputControllter.tsx
@@ -1,3 +1,27 @@
-export const AddressSearchInputControllter = () => {
-  return <div>AddressSearchInputControllter</div>;
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import AddressSearchInputModal from "@/components/shared/AddressSearchInputModal";
+
+interface Props<ControlType extends FieldValues> {
+  formFieldName: Path<ControlType>;
+  control: Control<ControlType>;
+  modalTitle?: string;
+  onClose: () => void;
+}
+
+export const AddressSearchInputControllter = <ControlType extends FieldValues>({
+  onClose,
+  formFieldName,
+  control,
+  modalTitle,
+}: Props<ControlType>) => {
+  return (
+    <>
+      <Controller
+        name={formFieldName}
+        control={control}
+        render={({ field }) => <AddressSearchInputModal modalTitle={modalTitle} {...field} onClose={onClose} />}
+      />
+    </>
+  );
 };
+export default AddressSearchInputControllter;
---
+++ src/components/shared/AddressSearchInputModal.tsx
@@ -0,0 +1,34 @@
+import { forwardRef, InputHTMLAttributes } from "react";
+
+import Spacing from "@/components/shared/Spacing";
+import Button from "@/components/shared/Button";
+import Text from "@/components/shared/Text";
+
+import classNames from "classnames/bind";
+import styles from "./InputModal.module.scss";
+const cx = classNames.bind(styles);
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  modalTitle: string;
+  buttonText?: string;
+  onClose: () => void;
+}
+
+export const AddressSearchInputModal = forwardRef<HTMLInputElement, Props>(
+  ({ modalTitle, buttonText, onClose, ...props }, ref) => {
+    return (
+      <div className={cx("back-drop")}>
+        <div className={cx("container")} aria-label="alert-modal">
+          <Spacing size={30} />
+          <Text className={cx("txt-title")} text={modalTitle} />
+          <Spacing size={30} />
+          <input className={cx("input")} ref={ref} maxLength={30} {...props} />
+          <Spacing size={30} />
+          <Button className={cx("close-button")} title={buttonText} onClick={onClose} />
+        </div>
+      </div>
+    );
+  },
+);
+
+export default AddressSearchInputModal;
