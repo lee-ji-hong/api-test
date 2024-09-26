00755483ce5d14b856e5c7bb2daf1e912da280b2
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-26 18:37:57 +0900

    Feat:"리포트 UI 작업 70%"

--- src/pages/ReportPage/index.tsx
+++ src/pages/ReportPage/index.tsx
@@ -15,6 +15,7 @@ import Text from "@/components/shared/Text";
 import { useInternalRouter } from "@/hooks/useInternalRouter";
 import { IMAGES } from "@/constants/images";
 import { formData } from "@/recoil/atoms";
+import { orderStep, warning } from "./data";
 import { MOCK } from "@/pages/DepositResultPage/mock";
 import classNames from "classnames/bind";
 import styles from "./ReportPage.module.scss";
@@ -126,7 +127,10 @@ const ReportPage = () => {
             <Spacing size={70} />
             <Text className={cx("txt-title")} text="전세 대출을 하게 되면/n한달에 외식을 10번을 더 줄여야해요!" />
             <Spacing size={8} />
-            <Text className={cx("txt-sub")} text="숨겨진 비용까지 철저히 분석하여/n현명한 결정을 할 수 있어요" />
+            <Text
+              className={cx("txt-sub")}
+              text="보증금 중 자기자금 1억원에 대해서는/n월 50만원의 기회비용이 발생해요 "
+            />
             <Spacing size={8} />
             <Image className={cx("img")} imageInfo={IMAGES?.Report_1} />
           </div>
@@ -139,7 +143,9 @@ const ReportPage = () => {
               highlight="#20대 인기상품 #초저금리 #최대한도"
             />
             <Spacing size={16} />
-            <div>박스 UI</div>
+            <div className={cx("reason-box")}>
+              <Text className={cx("txt-sub")} text={`${reportData.recommendationReason || "이 대출을 추천한 이유"}`} />
+            </div>
           </div>
           {/* Section06 */}
           <div className={cx("box")}>
@@ -155,10 +161,88 @@ const ReportPage = () => {
             <button className={cx("list-button")} onClick={handleDepositListToggle}>
               {showMoreDepositList ? "다른 상품 더 보기 ∧" : "다른 상품 더 보기 ∨"}
             </button>
-            <Text className={cx("txt-sub")} text="숨겨진 비용까지 철저히 분석하여/n현명한 결정을 할 수 있어요" />
-            <div>박스 UI</div>
           </div>
-          <Spacing size={60} />
+          {/* Section07 */}
+          <div className={cx("box")}>
+            <Spacing size={70} />
+            <Text className={cx("txt-title")} text="대출 불가 상품들의 사유를 확인해보세요!" />
+            <Spacing size={8} />
+            <DepositList
+              list={MOCK || reportData?.recommendedProducts}
+              isShow={true}
+              toggle={showMoreDepositList}
+              color="white"
+            />
+            <button className={cx("list-button")} onClick={handleDepositListToggle}>
+              {showMoreDepositList ? "다른 상품 더 보기 ∧" : "다른 상품 더 보기 ∨"}
+            </button>
+          </div>
+
+          {/* Section08 */}
+          <div className={cx("box")}>
+            <Spacing size={70} />
+            <Text className={cx("txt-title")} text="아래 은행에서 취급하는 상품이예요!" />
+            <Spacing size={8} />
+            {reportData?.availableBanks?.map((icon: string) => (
+              <div key={icon}>
+                <Text className={cx("txt-title")} text={icon} />
+              </div>
+            ))}
+          </div>
+          {/* Section09 */}
+          <div className={cx("box")}>
+            <Spacing size={70} />
+            <Text className={cx("txt-title")} text="필요한 서류는 이런 것들이 있어요!" />
+            <Spacing size={10} />
+            <div>
+              {[
+                "신분증",
+                "소득증명서",
+                "주택임대차증명서",
+                "세금 납부 증명서",
+                "신용 등급 확인서",
+                "주택등기부등본",
+                "기존대출내역서",
+                "대출상환증명서",
+                "통장거래내역서",
+              ].map((item, index) => (
+                <Badge2 className={cx("badge-icon")} key={index} title={item} />
+              ))}
+            </div>
+          </div>
+          {/* Section10 */}
+          <div className={cx("box")}>
+            <Spacing size={70} />
+            <Text className={cx("txt-title")} text="이런 순서로 대출 진행하면 OK!" />
+            <Spacing size={10} />
+            <div>
+              {orderStep.map((step) => (
+                <div className={cx("order-wrap")} key={step.id}>
+                  <Image className={cx("img")} imageInfo={IMAGES?.Report_1} />
+                  <div>
+                    <Text className={cx("txt-title")} text={`STEP ${step.id}`} />
+                    <Text className={cx("txt-sub")} text={step.description} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* Section11 */}
+          <div className={cx("box")}>
+            <Spacing size={70} />
+            <Text className={cx("txt-warning")} text="주의사항" />
+            <Spacing size={10} />
+            <ul>
+              {warning.map((item, index) => (
+                <li key={index} className={cx("warning-list")}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* 버튼 */}
+          <Spacing size={100} />
           <Button className={cx("button-wrap")} onClick={handleCancelClick} title="전월세 대출 상품 확인하러 가기" />
           <Spacing size={14} />
         </div>
