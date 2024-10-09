001b2c3f4649ce6621f15505effa7ae962962e6c
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-09 14:26:41 +0900

    Feat: "로그아웃, 회원 탈퇴 리스트 로그인 시에만 노출되도록 수정"

--- src/pages/SettingPage/index.tsx
+++ src/pages/SettingPage/index.tsx
@@ -7,6 +7,8 @@ import List from "@/components/shared/List";
 import { SETTINGS_MENU } from "@/constants/SettingsMenu";
 import { useInternalRouter } from "@/hooks/useInternalRouter";
 import { useSendLogout } from "@/hooks/queries/useSendLogout";
+import { useAuth } from "@/hooks/useAuth";
+
 import styles from "./settings.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
@@ -16,6 +18,7 @@ export default function SettingPage() {
   const [modalType, setModalType] = useState("");
   const { Logout } = useSendLogout();
   const router = useInternalRouter();
+  const { auth } = useAuth();
 
   const handleClick = (item: (typeof SETTINGS_MENU)[number]) => {
     if (item.isMailLink && item.mailTo) {
@@ -47,7 +50,7 @@ export default function SettingPage() {
       <Header className={cx("header")} title="설정" onLeftClick={() => router.goBack()} left="Back_btn" />
       <Spacing size={53} />
       <List className={cx("list-container")}>
-        {SETTINGS_MENU.map((item) => (
+        {SETTINGS_MENU.filter((item) => !(item.name === "로그아웃" || item.name === "회원탈퇴") || auth).map((item) => (
           <List.Row
             key={item.name}
             className={cx("list-content")}
