9d3b1184e7c435b8c5704648e658e71c9fb767d4
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-09 15:44:21 +0900

    Fix: "이메일 추가"

--- src/constants/SettingsMenu.ts
+++ src/constants/SettingsMenu.ts
@@ -2,7 +2,7 @@ export const SETTINGS_MENU = [
   { name: "서비스 소개", path: "/" },
   { name: "서비스 이용약관", path: "/terms-of-service" },
   { name: "개인정보 처리방침", path: "/privacy-policy" },
-  { name: "문의하기", isMailLink: true, mailTo: "mailto:example@example.com" },
+  { name: "문의하기", isMailLink: true, mailTo: "mailto:gw8413@gmail.com" },
   { name: "로그아웃" },
   { name: "회원탈퇴" },
 ];
