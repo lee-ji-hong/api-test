4fd09a64eb4a8eded7cbc82a7856d4c31fedb7f8
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-09 15:41:54 +0900

    Feat: "개인정보 처리방침 페이지 추가"

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -23,7 +23,7 @@ const CommunityPage = lazy(() => import("./CommunityPage"));
 const LoanInfoEntryPage = lazy(() => import("./LoanInfoEntryPage"));
 const ReportPage = lazy(() => import("./ReportPage"));
 const TermsOfServicePage = lazy(() => import("./TermsOfServicePage"));
-// const PrivacyPolicyPage = lazy(() => import("./PrivacyPolicyPage"));
+const PrivacyPolicyPage = lazy(() => import("./PrivacyPolicyPage"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -55,7 +55,7 @@ function App() {
                 <Route path="community/modify" element={<CommunityModifyPage />} />
                 <Route path="loan-info-entry" element={<LoanInfoEntryPage />} />
                 <Route path="terms-of-service" element={<TermsOfServicePage />} />
-                {/* <Route path="privacy-policy" element={<PrivacyPolicyPage />} /> */}
+                <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
 
                 <Route path="community/recent-report" element={<CommunityRecentReportPage />} />
                 <Route path="report" element={<ReportPage />} />
