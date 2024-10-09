2ca5cd82220f4f65d6f75be3906a3781125dc8d4
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-10 04:19:12 +0900

    Fix: "헬멧 위치 이동"

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,5 +1,7 @@
 import { Outlet, useLocation } from "react-router-dom";
 import MobileTabBar from "@/components/shared/MobileTabBar";
+import { Helmet } from "react-helmet-async";
+
 import styles from "./AppLayout.module.scss";
 import classNames from "classnames/bind";
 import { useRecoilValue, useSetRecoilState } from "recoil";
@@ -18,6 +20,17 @@ const AppLayout = () => {
 
   return (
     <>
+      <Helmet>
+        <title>뱅칼 | 전월세대출조회</title>
+        <meta name="description" content="당신을 위한 최적의 전세 대출을 찾아드릴게요 - 전세대출, 신혼, 청년" />
+        <meta name="keywords" content="내집플랜,전세대출,신혼,청년,대출" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Readme Monster" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://myzipplan.com/metaThum.png" />
+        <meta property="og:article:author" content="내집플랜" />
+        <meta property="og:url" content="https://myzipplan.com/" />
+      </Helmet>
       {isLoading && <div className={cx("loading")}>로딩 중...</div>} {/* 로딩 컴포넌트 */}
       {pathname === "/deposit-entry" || pathname === "/community" ? (
         <>
