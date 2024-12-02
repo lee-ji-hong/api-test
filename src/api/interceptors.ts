83c04f78c2b3841f602fe7aa9703793636429a39
진정한 <g.kmwdev@gmail.com>
2024-12-02 22:27:36 +0900

    Feat: "애널리틱스 이벤트 추가"

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,4 +1,5 @@
 import { LOGIN_REDIRECT } from "@/constants/loginLanding";
+import { useLogEvent } from "@/utils/firebaseLogEvent";
 import { setAdviceReportData, setCommunityIdAfterLogin, setLoginRedirectPath } from "@/utils/localStorage";
 import { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
 import { getCookie } from "./authUtils";
