03a6e1bd68f672b17b39f39908a191e51b0fbd4c
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-02-28 00:49:03 +0900

    Feat: "LoanResult ui"

--- src/pages/LoanInfoEntryTypeBPage/index.tsx
+++ src/pages/LoanInfoEntryTypeBPage/index.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/sections/Header";
 import Spacing from "@/components/shared/Spacing";
-import { ProgressPercentage, GlobalPortal } from "@/components";
+import { ProgressPercentage } from "@/components";
 
 import { useInternalRouter } from "@/hooks/useInternalRouter";
 
 import classNames from "classnames/bind";
 import styles from "./LoanInfoEntryTypeBPage.module.scss";
+import { LoanResult } from "./LoanResult";
 const cx = classNames.bind(styles);
 
 export const LoanInfoEntryTypeBPage = () => {
@@ -13,12 +14,14 @@ export const LoanInfoEntryTypeBPage = () => {
 
   return (
     <>
-      <GlobalPortal.Consumer>
-        <ProgressPercentage />
-      </GlobalPortal.Consumer>
+      {/* <GlobalPortal.Consumer>
+      </GlobalPortal.Consumer> */}
+      <ProgressPercentage />
       <Header className={cx("cancel")} onLeftClick={() => router.push("/deposit-result")} left="Back_btn" />
       <Spacing size={53} />
-      <div className={cx("container")}></div>
+      <div className={cx("container")}>
+        <LoanResult />
+      </div>
     </>
   );
 };
