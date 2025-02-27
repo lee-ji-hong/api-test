5b7c48538624fc3116bcf8630951ab3022263926
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-02-27 15:53:26 +0900

    Fix: "권한 수정"

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -60,7 +60,6 @@ class Axios {
       if (token) headers.AccessToken = token;
     }
 
-    // 테스트
     // 추가적인 헤더들을 덮어씌우기
     if (additionalHeaders) {
       Object.assign(headers, additionalHeaders);
