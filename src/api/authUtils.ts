91f747875acde0e96f42644be213ae26f6a97800
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-03-09 00:37:05 +0900

    Feat: "removeCookie hook 생성"

--- src/api/authUtils.ts
+++ src/api/authUtils.ts
@@ -14,3 +14,7 @@ export const getCookie = (name: string): string | null => {
 export const setCookie = (name: string, value: string): void => {
   document.cookie = `${name}=${encodeURIComponent(value)};`;
 };
+
+export const removeCookie = (name: string): void => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
