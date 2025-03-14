f907d51db45c5e74e1caea3e51aca40ce41c0137
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-03-14 15:13:15 +0900

    Feat: "계산기 뱃지 추가"

--- src/components/sections/LoanInfoEntryTypeB/InputController.tsx
+++ src/components/sections/LoanInfoEntryTypeB/InputController.tsx
@@ -2,10 +2,8 @@ import { Control, Controller, FieldValues, Path } from "react-hook-form";
 
 import { GlobalPortal } from "@/components/shared/GlobalPortal";
 import KeyboardModal from "@/components/modal/KeyboardModal";
-import Spacing from "@/components/shared/Spacing";
-import Badge from "@/components/shared/Badge";
 import Input from "@/components/shared/Input";
-import { OptionItem, OptionsType } from "@/models";
+import { MONEY } from "@/constants/money";
 
 import styles from "@/pages/CalculatorPage/CalculatorPage.module.scss";
 import classNames from "classnames/bind";
@@ -24,7 +22,6 @@ interface Props<ControlType extends FieldValues> {
   keyboardHeight: number;
   onFocus?: () => void;
   onBlur?: () => void;
-  options?: OptionItem[] | OptionsType;
   min?: Limit;
   max?: Limit;
   unit?: string;
@@ -36,16 +33,12 @@ const InputController = <ControlType extends FieldValues>({
   formFieldName,
   keyboardHeight,
   control,
-  options,
   min,
   max,
   unit,
   onFocus,
   onBlur,
 }: Props<ControlType>) => {
-  const isOptionItemArray = (options: OptionItem[] | OptionsType | undefined): options is OptionItem[] => {
-    return Array.isArray(options);
-  };
   return (
     <Controller
       name={formFieldName}
@@ -53,7 +46,7 @@ const InputController = <ControlType extends FieldValues>({
       render={({ field }) => {
         const isInvalidValue = field.value <= (min?.value ?? -1) || field.value > (max?.value ?? Infinity);
         const warningMessage = field.value <= (min?.value ?? -1) ? (min?.ment ?? "") : (max?.ment ?? "");
-
+        console.log(userValue, field);
         // 키보드 입력 이벤트
         const handleKeyPress = (key: string) => {
           if (key === "⌫") {
@@ -70,27 +63,20 @@ const InputController = <ControlType extends FieldValues>({
           }
         };
 
-        // 금액 뱃지 이벤트
         const handleBadgeClick = (label: string) => {
-          if (isOptionItemArray(options)) {
-            const item = options?.find((item) => item.label === label);
-            if (item && typeof item.value === "number") {
-              const currentValue = field.value || 0;
-              const newValue = currentValue + item.value;
-              field.onChange(Math.min(newValue, 99999999));
-            }
+          const filteredMoney = field.name in MONEY ? MONEY[field.name as keyof typeof MONEY] : MONEY.default;
+          const item = filteredMoney.find((item) => item.label === label)!;
+          if (item) {
+            const currentValue = field.value || 0;
+            const newValue = currentValue + item.value;
+            field.onChange(Math.min(newValue, 99999999));
           }
-        };
-
-        const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, label: string) => {
-          e.preventDefault();
-          handleBadgeClick(label);
         };
 
         return (
           <div className={cx("input-container")}>
             <Input
-              id={id}
+              id={id.toString()}
               error={isInvalidValue ? true : false}
               value={userValue ?? field.value}
               onChange={field.onChange}
@@ -99,26 +85,17 @@ const InputController = <ControlType extends FieldValues>({
               onBlur={onBlur}
               unit={unit}
               ref={field.ref}
-              // {...field}
             />
-            <Spacing size={12} />
-            <div className={cx("badge-container")}>
-              {isOptionItemArray(options) &&
-                options?.map(({ label, value }) => (
-                  <Badge
-                    className={cx("button")}
-                    key={value.toString()}
-                    title={label}
-                    onClick={(e) => handleClick(e, label)}
-                  />
-                ))}
-            </div>
+
             {keyboardHeight > 0 && (
               <GlobalPortal.Consumer>
                 <KeyboardModal
                   className={cx("keyboard-container")}
                   onKeyPress={handleKeyPress}
+                  isBadge={true}
+                  handleBadgeClick={handleBadgeClick}
                   keyboardHeight={keyboardHeight}
+                  name={field.name}
                 />
               </GlobalPortal.Consumer>
             )}
