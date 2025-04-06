9537d2fea35d876465ce9a69860eb778b61b7f62
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-04-06 17:05:03 +0900

    Fix: "신생아 포함여부 버튼 위치 수정"

--- src/components/sections/LoanInfoEntryTypeB/UserInfo.tsx
+++ src/components/sections/LoanInfoEntryTypeB/UserInfo.tsx
@@ -1,83 +1,90 @@
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
-import { useState } from "react";
-import { useRecoilState } from "recoil";
-
 import Image from "@/components/shared/Image";
 import Text from "@/components/shared/Text";
-import { formData } from "@/recoil/atoms";
 import { IMAGES } from "@/constants/images";
+import { formData } from "@/recoil/atoms";
 import { ChildStatus, sendLoanAdviceReportRequest } from "@/models";
-
+import { useRecoilState } from "recoil";
 import classNames from "classnames/bind";
 import styles from "@/pages/LoanInfoEntryTypeBPage/LoanInfoEntryTypeBPage.module.scss";
 const cx = classNames.bind(styles);
 
-interface UserInfoProps<ControlType extends FieldValues> {
+interface Props<ControlType extends FieldValues> {
   formFieldName: Path<ControlType>;
   control: Control<ControlType>;
   modalTitle?: string;
-  buttonText: string;
+  modalSubTitle?: string;
+  options?: { label: string; value: boolean | string }[];
+  onBlur: () => void;
 }
 
-const statusLabels: Record<ChildStatus, string> = {
-  NO_CHILD: "무자녀",
-  ONE_CHILD: "1자녀",
-  TWO_CHILD: "2자녀",
-  THREE_OR_MORE_CHILDREN: "3자녀 이상",
-};
-
-export const UserInfo = <ControlType extends FieldValues>({ formFieldName, control }: UserInfoProps<ControlType>) => {
+export const UserInfo = <ControlType extends FieldValues>({
+  onBlur,
+  formFieldName,
+  control,
+  options,
+}: Props<ControlType>) => {
   const [recoilFormData, setRecoilFormData] = useRecoilState<sendLoanAdviceReportRequest>(formData);
-  const [childStatus, setChildStatus] = useState("");
-
-  const handleCheckboxChange = () => {
-    if (childStatus === "NO_CHILD") {
-      alert("자녀를 선택해주세요");
-    } else {
-      setRecoilFormData((prevState) => ({
-        ...prevState,
-        hasNewborn: !prevState.hasNewborn,
-      }));
-    }
-  };
   return (
     <>
       <Controller
         name={formFieldName}
         control={control}
-        render={({ field }) => (
-          <>
-            <ul className={cx("options")}>
-              {(["NO_CHILD", "ONE_CHILD", "TWO_CHILD", "THREE_OR_MORE_CHILDREN"] as ChildStatus[]).map((status) => (
-                <li
-                  key={status}
-                  className={cx("option-button", {
-                    selected: field.value === status || (field.value === undefined && status === "NO_CHILD"),
-                  })}
-                  onClick={() => {
-                    console.log(field.value);
-                    console.log(status);
-                    field.onChange(status);
-                    setChildStatus(status);
-                    if (status === "NO_CHILD") {
-                      setRecoilFormData((prevState) => ({
-                        ...prevState,
-                        hasNewborn: false,
-                      }));
-                    }
-                  }}>
-                  {statusLabels[status]}
-                </li>
-              ))}
-            </ul>
-            <div className={cx("checkbox-container")} onClick={handleCheckboxChange}>
-              <Image className={cx("img", `check-${recoilFormData.hasNewborn}`)} imageInfo={IMAGES?.Checkbox_btn} />
-              <div>
-                <Text text="신생아 포함 여부" />
+        render={({ field }) => {
+          const handleSelect = (value: boolean | string) => {
+            field.onChange(value);
+            if (value === "NO_CHILD") {
+              setRecoilFormData((prevState) => ({
+                ...prevState,
+                childStatus: value as ChildStatus,
+                hasNewborn: false,
+              }));
+            }
+            onBlur();
+          };
+
+          const handleCheckboxChange = (value: string) => {
+            if (value === "NO_CHILD" || value === undefined) {
+              setRecoilFormData((prevState) => ({
+                ...prevState,
+                hasNewborn: false,
+              }));
+              alert("자녀를 선택해주세요");
+            } else {
+              setRecoilFormData((prevState) => ({
+                ...prevState,
+                childStatus: value as ChildStatus,
+                hasNewborn: !prevState.hasNewborn,
+              }));
+            }
+          };
+
+          return (
+            <>
+              <ul className={cx("options")}>
+                {options?.map(({ label, value }) => (
+                  <li
+                    key={value.toString()}
+                    className={cx("option-button", {
+                      selected: field.value === value,
+                    })}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleSelect(value);
+                    }}>
+                    {label}
+                  </li>
+                ))}
+              </ul>
+              <div className={cx("checkbox-container")} onClick={() => handleCheckboxChange(field.value)}>
+                <Image className={cx("img", `check-${recoilFormData.hasNewborn}`)} imageInfo={IMAGES?.Checkbox_btn} />
+                <div>
+                  <Text text="신생아 포함 여부" />
+                </div>
               </div>
-            </div>
-          </>
-        )}
+            </>
+          );
+        }}
       />
     </>
   );
